e70eac4f0018b9a6ca4e77e20f07e33362eeabe5
Phil <phil@evolvedigital.co.uk>
2020-08-26 22:00:44 +0100

    Add animations to social links

--- app.js
+++ app.js
@@ -2,15 +2,27 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const social = document.querySelector('.social-links');
     // get all nav links
     const navLinks = document.querySelectorAll('.nav-links li');
+    const socialLinks = document.querySelectorAll('.social-links a');
 
     // Toggle nav
     burger.addEventListener('click', function() {
         nav.classList.toggle('nav-active');
+        social.classList.toggle('nav-active');
         
         // Animate links
         navLinks.forEach((link, index) => {
+            // console.log(index);
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`; // add delay at end
+            }
+        });
+
+        socialLinks.forEach((link, index) => {
             // console.log(index);
             if (link.style.animation) {
                 link.style.animation = '';
